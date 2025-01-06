b0b52b17b8c054cef9382da5f2121ce7aadcbce9
Ahmet Bakırcan <ahmetbakircan@gmail.com>
2025-01-06 13:49:37 +0300

    Fixed some things

---
+++ libft/ft_in_charset.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_in_charset.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 13:48:47 by abakirca          #+#    #+#             */
+/*   Updated: 2025/01/06 13:48:48 by abakirca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_in_charset(char const c, char const *charset)
+{
+	int	i_charset;
+
+	i_charset = 0;
+	while (charset[i_charset])
+	{
+		if (c == charset[i_charset++])
+			return (1);
+	}
+	return (0);
+}
--- src/parse.c
+++ src/parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbaypara <mbaypara@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 12:53:26 by mbaypara          #+#    #+#             */
-/*   Updated: 2025/01/04 19:20:07 by mbaypara         ###   ########.fr       */
+/*   Updated: 2025/01/06 13:43:17 by abakirca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,56 +24,111 @@ static t_scene	*init_scene(t_scene *scene)
 	return (scene);
 }
 
-static t_scene	*temp_scene(t_scene *scene)
+// static t_scene	*temp_scene(t_scene *scene)
+// {
+// 	t_camera	*camera = ft_calloc(1, sizeof(t_camera));
+// 	t_light		*light = ft_calloc(1, sizeof(t_light));
+// 	t_sphere	*sphere = ft_calloc(1, sizeof(t_sphere));
+// 	t_cylinder	*cylinder = ft_calloc(1, sizeof(t_cylinder));
+// 	t_plane		*plane = ft_calloc(1, sizeof(t_plane));
+
+// 	camera->position = (t_vector3){0, 0, 0};
+// 	camera->fov = 90;
+
+// 	light->position = (t_vector3){-55, 0, 0};
+// 	light->ratio = 0.9 * 255;
+// 	light->color = *mult_color_d((t_color){255, 255, 255}, light->ratio);
+
+// 	sphere->pos = (t_vector3){0, 0, -155};
+// 	sphere->radius = 50 * 50;
+// 	sphere->color = (t_color){255, 255, 255};
+
+// 	scene->al_color = *mult_color_d((t_color){255, 255, 255}, 0.3);
+
+// 	cylinder->pos = (t_vector3){5, 0, -15};
+// 	cylinder->dir = vec3_norm((t_vector3){-9, -5, -6});
+// 	cylinder->radius = 4 / 2;
+// 	cylinder->radius = cylinder->radius * cylinder->radius;
+// 	cylinder->height = 5;
+// 	cylinder->pos2 = vec3_add(cylinder->pos, vec3_mult(cylinder->dir, cylinder->height));
+// 	cylinder->color = (t_color){10, 0, 255};
+
+// 	plane->pos = (t_vector3){0, -5, 0};
+// 	plane->normal = vec3_norm((t_vector3){0, 0.1, 0});
+// 	plane->color = (t_color){255, 255, 255};
+
+// 	ft_lstadd_front(&(scene->cameras), ft_lstnew(camera));
+// 	ft_lstadd_front(&(scene->lights), ft_lstnew(light));
+// 	ft_lstadd_front(&(scene->spheres), ft_lstnew(sphere));
+// 	ft_lstadd_front(&(scene->cylinders), ft_lstnew(cylinder));
+// 	ft_lstadd_front(&(scene->planes), ft_lstnew(plane));
+// 	return (scene);
+// }
+
+// t_scene	*parsing(int fd)
+// {
+// 	t_scene	*scene;
+// 	char	*line;
+// 	char	**data;
+
+// 	scene = init_scene(ft_calloc(1, sizeof(t_scene)));
+// 	//line = get_next_line(fd);
+// 	return (temp_scene(scene));
+// }
+
+
+int	check_line(const char *line, char **data, const char *type, const int nb_elements)
 {
-	t_camera	*camera = ft_calloc(1, sizeof(t_camera));
-	t_light		*light = ft_calloc(1, sizeof(t_light));
-	t_sphere	*sphere = ft_calloc(1, sizeof(t_sphere));
-	t_cylinder	*cylinder = ft_calloc(1, sizeof(t_cylinder));
-	t_plane		*plane = ft_calloc(1, sizeof(t_plane));
-
-	camera->position = (t_vector3){0, 0, 0};
-	camera->fov = 90;
-
-	light->position = (t_vector3){-55, 0, 0};
-	light->ratio = 0.9 * 255;
-	light->color = *mult_color_d((t_color){255, 255, 255}, light->ratio);
-
-	sphere->pos = (t_vector3){0, 0, -155};
-	sphere->radius = 50 * 50;
-	sphere->color = (t_color){255, 255, 255};
-
-	scene->al_color = *mult_color_d((t_color){255, 255, 255}, 0.3);
-
-	cylinder->pos = (t_vector3){5, 0, -15};
-	cylinder->dir = vec3_norm((t_vector3){-9, -5, -6});
-	cylinder->radius = 4 / 2;
-	cylinder->radius = cylinder->radius * cylinder->radius;
-	cylinder->height = 5;
-	cylinder->pos2 = vec3_add(cylinder->pos, vec3_mult(cylinder->dir, cylinder->height));
-	cylinder->color = (t_color){10, 0, 255};
-
-	plane->pos = (t_vector3){0, -5, 0};
-	plane->normal = vec3_norm((t_vector3){0, 0.1, 0});
-	plane->color = (t_color){255, 255, 255};
-
-	ft_lstadd_front(&(scene->cameras), ft_lstnew(camera));
-	ft_lstadd_front(&(scene->lights), ft_lstnew(light));
-	ft_lstadd_front(&(scene->spheres), ft_lstnew(sphere));
-	ft_lstadd_front(&(scene->cylinders), ft_lstnew(cylinder));
-	ft_lstadd_front(&(scene->planes), ft_lstnew(plane));
-	return (scene);
+	if (!ft_strcmp(data[0], type))
+	{
+		if (ft_in_charset(line[ft_strlen(type)], " \t"))
+			return (ft_tab_size(data) == nb_elements);
+	}
+	return (0);
 }
 
-t_scene	*parsing(int fd)
+t_scene	*parse(int fd)
 {
 	t_scene	*scene;
 	char	*line;
+	int		ret;
 	char	**data;
 
-	scene = init_scene(ft_calloc(1, sizeof(t_scene)));
-	//line = get_next_line(fd);
-	return (temp_scene(scene));
+	if (!(scene = malloc(sizeof(*scene))))
+		print_err_and_exit("Malloc failed", 1);
+	if (!(init_scene(scene)))
+		return (NULL);
+	while ((ret = get_next_line(&line, fd)) == 1)
+	{
+		data = ft_split_set((*line ? line : "iamcheating"), WHITE_SPACES);
+		if (check_line(line, data, "R", NB_ELEM_RESOLUTION) && !scene->resolution.w)
+			set_resolution(scene, data);
+		else if (check_line(line, data, "A", NB_ELEM_AL) && !scene->al.ratio)
+			set_ambient_light(scene, data);
+		else if (check_line(line, data, "c", NB_ELEM_CAMERA))
+			set_camera(scene, data);
+		else if (check_line(line, data, "l", NB_ELEM_LIGHT))
+			set_light(scene, data);
+		else if (check_line(line, data, "sp", NB_ELEM_SPHERE))
+			set_sphere(scene, data);
+		else if (check_line(line, data, "pl", NB_ELEM_PLANE))
+			set_plane(scene, data);
+		else if (check_line(line, data, "sq", NB_ELEM_SQUARE))
+			set_square(scene, data);
+		else if (check_line(line, data, "cy", NB_ELEM_CYLINDER) || check_line(line, data, "cy", NB_ELEM_CYLINDER + 1))
+			set_cylinder(scene, data);
+		else if (check_line(line, data, "tr", NB_ELEM_TRIANGLE))
+			set_triangle(scene, data);
+		else if (!ft_is_from_charset(line, WHITE_SPACES))
+		{
+			free(line);
+			free(data);
+			print_err_and_exit("Parse error", PARSE_ERROR);
+		}
+		free(line);
+		free(data);
+	}
+	return (scene);
 }
 
 t_scene	*parse_scene(int argc, char **av)
