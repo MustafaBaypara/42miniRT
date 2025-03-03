e6ab95d8ad9d153194025532e753270dacd684e4
Ahmet Bakırcan <ahmetbakircan@gmail.com>
2025-03-03 19:35:07 +0300

    cylinder comments

--- src/cylinder_utils.c
+++ src/cylinder_utils.c
@@ -6,7 +6,7 @@
 /*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 15:30:29 by mbaypara          #+#    #+#             */
-/*   Updated: 2025/02/25 21:08:38 by abakirca         ###   ########.fr       */
+/*   Updated: 2025/03/03 15:08:11 by abakirca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ double	solve_pl(t_vector3 origin, t_vector3 dir, t_vector3 pos, t_vector3 n)
 	return (INFINITY); // if the distance is less than EPSILON, return INFINITY
 }
 
-// solve_cyl calculates the intersection of the ray and the side of the cylinder
 int	solve_cyl(double a[2], t_ray ray, t_cylinder cyl)
 {
 	t_vector3	v;
@@ -35,17 +34,22 @@ int	solve_cyl(double a[2], t_ray ray, t_cylinder cyl)
 	double		i;
 	double		j;
 	double		k;
-
-	v = vec3_mult(cyl.dir, dot_pd(ray.dir, cyl.dir)); // get the projection of the direction of the ray on the direction of the cylinder
-	v = vec3_sub(ray.dir, v); // get the vector from the projection of the direction of the ray on the direction of the cylinder to the direction of the ray
-	u = vec3_mult(cyl.dir, dot_pd(vec3_sub(ray.origin, cyl.pos), cyl.dir)); // get the projection of the vector from the origin of the ray to the position of the cylinder on the direction of the cylinder
-	u = vec3_sub(vec3_sub(ray.origin, cyl.pos), u); // get the vector from the projection of the vector from the origin of the ray to the position of the cylinder on the direction of the cylinder to the vector from the origin of the ray to the position of the cylinder
-	i = dot_pd(v, v); // get the dot product of the vector from the projection of the direction of the ray on the direction of the cylinder to the direction of the ray
-	j = 2 * dot_pd(v, u); // get the dot product of the vector from the projection of the direction of the ray on the direction of the cylinder to the direction of the ray and the vector from the projection of the vector from the origin of the ray to the position of the cylinder on the direction of the cylinder to the vector from the origin of the ray to the position of the cylinder
-	k = dot_pd(u, u) - pow(cyl.radius, 2); // get the dot product of the vector from the projection of the vector from the origin of the ray to the position of the cylinder on the direction of the cylinder to the vector from the origin of the ray to the position of the cylinder and the vector from the projection of the vector from the origin of the ray to the position of the cylinder on the direction of the cylinder to the vector from the origin of the ray to the position of the cylinder minus the square of the radius of the cylinder
-	a[0] = (-j + sqrt(pow(j, 2) - 4 * i * k)) / (2 * i); // solve the quadratic equation
-	a[1] = (-j - sqrt(pow(j, 2) - 4 * i * k)) / (2 * i); // solve the quadratic equation
-	if (a[0] < EPSILON && a[1] < EPSILON) // if both a[0] and a[1] are less than EPSILON, return 0
+	// ray direction perpendicular to the cylinder axis.
+	v = vec3_mult(cyl.dir, dot_pd(ray.dir, cyl.dir));
+	v = vec3_sub(ray.dir, v);
+	// Vector from cylinder axis to ray origin, perpendicular to axis.
+	u = vec3_mult(cyl.dir, dot_pd(vec3_sub(ray.origin, cyl.pos), cyl.dir));
+	u = vec3_sub(vec3_sub(ray.origin, cyl.pos), u);
+	// calculate the coefficients of the quadratic equation
+	i = dot_pd(v, v);
+	j = 2 * dot_pd(v, u);
+	k = dot_pd(u, u) - pow(cyl.radius, 2);
+	// i * t^2 + j * t + k = 0
+	// t = (-j ± √(j^2 - 4ik)) / (2i)
+	a[0] = (-j + sqrt(pow(j, 2) - 4 * i * k)) / (2 * i);
+	a[1] = (-j - sqrt(pow(j, 2) - 4 * i * k)) / (2 * i);
+	// if the discriminant is less than 0, return 0 which means the ray does not hit the cylinder
+	if (a[0] < EPSILON && a[1] < EPSILON)
 		return (0);
 	return (1);
 }
@@ -69,47 +73,57 @@ void	calc_normal(double a[2], t_cylinder cyl, double d1, double d2)
 	a[0] = i;
 }
 
-// isec_cap calculates the intersection of the ray and the top cap of the cylinder
+
 double	isec_cap(t_ray ray, t_cylinder cyl, double d1, double d2)
 {
 	t_vector3	p1;
 	t_vector3	p2;
 	t_vector3	cap;
 
-	cap = vec3_add(cyl.pos, vec3_mult(cyl.dir, cyl.height)); // get the position of the top cap of the cylinder
-	d1 = solve_pl(ray.origin, ray.dir, cyl.pos, cyl.dir); // solve the intersection of the ray and the bottom cap of the cylinder
-	d2 = solve_pl(ray.origin, ray.dir, cap, cyl.dir); // solve the intersection of the ray and the top cap of the cylinder
-	if (d1 < INFINITY && d2 < INFINITY) // if the distance is less than INFINITY
+	// get the position of the top cap
+	cap = vec3_add(cyl.pos, vec3_mult(cyl.dir, cyl.height));
+	// get the distance between the origin of the ray and the base cap
+	d1 = solve_pl(ray.origin, ray.dir, cyl.pos, cyl.dir);
+	// get the distance between the origin of the ray and the top cap
+	d2 = solve_pl(ray.origin, ray.dir, cap, cyl.dir);
+	// if both d1 and d2 are less than INFINITY which means the ray intersects the base and top cap
+	if (d1 < INFINITY && d2 < INFINITY)
 	{
-		p1 = vec3_add(ray.origin, vec3_mult(ray.dir, d1)); // get the point of impact
-		p2 = vec3_add(ray.origin, vec3_mult(ray.dir, d2)); // get the point of impact
+		// get the point of hit on the base and top cap
+		p1 = vec3_add(ray.origin, vec3_mult(ray.dir, d1));
+		p2 = vec3_add(ray.origin, vec3_mult(ray.dir, d2));
+		// checks if the hit points p1 and p2 are within the circular caps
+		// if they are, return the minimum distance between the base and top cap
 		if ((d1 < INFINITY && distance(p1, cyl.pos) <= cyl.radius)
-			&& (d2 < INFINITY && distance(p2, cap) <= cyl.radius)) // if the distance is less than INFINITY and the distance between the point of impact and the position of the cap is less than the radius of the cylinder
-			return (get_minf(d1, d2)); // return the minimum value between d1 and d2
-		else if (d1 < INFINITY && distance(p1, cyl.pos) <= cyl.radius) // if the distance is less than INFINITY and the distance between the point of impact and the position of the cap is less than the radius of the cylinder
-			return (d1); // return d1
-		else if (d2 < INFINITY && distance(p2, cap) <= cyl.radius) // if the distance is less than INFINITY and the distance between the point of impact and the position of the cap is less than the radius of the cylinder
-			return (d2); // return d2
+			&& (d2 < INFINITY && distance(p2, cap) <= cyl.radius))
+			return (get_minf(d1, d2));
+		// if the hit point p1 is within the base cap, return the distance between the origin of the ray and the base cap
+		else if (d1 < INFINITY && distance(p1, cyl.pos) <= cyl.radius)
+			return (d1);
+		// if the hit point p2 is within the top cap, return the distance between the origin of the ray and the top cap
+		else if (d2 < INFINITY && distance(p2, cap) <= cyl.radius)
+			return (d2);
 	}
-	return (INFINITY); // if the distance is greater than INFINITY, return INFINITY
+	// if neither the base cap nor the top cap is hit, return INFINITY
+	return (INFINITY);
 }
 
-// isec_side calculates the intersection of the ray and the side of the cylinder
+
 double	isec_side(t_ray ray, t_cylinder cyl)
 {
 	double	a[2];
 	double	d1;
 	double	d2;
 
-	if (!solve_cyl(a, ray, cyl)) // check if the ray hits the side of the cylinder
+	if (!solve_cyl(a, ray, cyl))
 		return (INFINITY);
 	d1 = dot_pd(cyl.dir, vec3_sub(vec3_mult(ray.dir, a[0]),
-				vec3_sub(cyl.pos, ray.origin))); // get the dot product of the direction of the cylinder and the vector from the origin of the ray to the point of impact
+				vec3_sub(cyl.pos, ray.origin)));
 	d2 = dot_pd(cyl.dir, vec3_sub(vec3_mult(ray.dir, a[1]),
-				vec3_sub(cyl.pos, ray.origin))); // get the dot product of the direction of the cylinder and the vector from the origin of the ray to the point of impact
+				vec3_sub(cyl.pos, ray.origin)));
 	if (!((d1 >= 0 && d1 <= cyl.height && a[0] > EPSILON)
-			|| (d2 >= 0 && d2 <= cyl.height && a[0] > EPSILON))) // if the dot product is not between 0 and the height of the cylinder and the distance is less than EPSILON
+			|| (d2 >= 0 && d2 <= cyl.height && a[0] > EPSILON)))
 		return (INFINITY);
-	calc_normal(a, cyl, d1, d2); // calculate the normal of the cylinder
-	return (a[0]); // return the distance
+	calc_normal(a, cyl, d1, d2);
+	return (a[0]);
 }
--- src/e_cylinder.c
+++ src/e_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 14:33:19 by mbaypara          #+#    #+#             */
-/*   Updated: 2025/02/25 21:05:58 by abakirca         ###   ########.fr       */
+/*   Updated: 2025/03/03 12:58:46 by abakirca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,17 @@ static double	cylinder_intersection(t_ray ray, t_cylinder cyl, int *is_side)
 	double	cy_inter;
 	double	cap_inter;
 
-	cy_inter = isec_side(ray, cyl); // check if the ray hits the side of the cylinder
-	cap_inter = isec_cap(ray, cyl, 0, 0); // check if the ray hits the top cap of the cylinder
-	if (cy_inter < INFINITY || cap_inter < INFINITY) // if the ray hits the side or the top cap of the cylinder
+	cy_inter = isec_side(ray, cyl);
+	cap_inter = isec_cap(ray, cyl, 0, 0);
+	if (cy_inter < INFINITY || cap_inter < INFINITY)
 	{
-		if (cy_inter < cap_inter) // if the ray hits the side of the cylinder
-			return (*is_side = 1, cy_inter); // return the side intersection
-		return (cap_inter); // if the ray hits the top cap of the cylinder, return the top cap intersection
+		if (cy_inter < cap_inter)
+			return (*is_side = 1, cy_inter);
+		return (cap_inter);
 	}
-	return (INFINITY); // if the ray does not hit the cylinder, return INFINITY
+	return (INFINITY);
 }
 
-// closest_point gets the closest point on the cylinder to the point of impact
 static t_vector3	closest_point(t_vector3 A, t_vector3 B, t_vector3 P)
 {
 	t_vector3	a_p;
@@ -37,24 +36,23 @@ static t_vector3	closest_point(t_vector3 A, t_vector3 B, t_vector3 P)
 	double		ap;
 	double		t;
 
-	a_b = vec3_sub(B, A); // get the vector from A to B
-	ab2 = dot_pd(a_b, a_b); // get the dot product of the vector from A to B
-	a_p = vec3_sub(P, A); // get the vector from A to P
-	ap = dot_pd(a_p, a_b); // get the dot product of the vector from A to P and the vector from A to B
-	t = ap / ab2; // get the ratio of the dot product of the vector from A to P and the vector from A to B to the dot product of the vector from A to B
-	if (t < 0) // if the ratio is less than 0, set the ratio to 0
+	a_b = vec3_sub(B, A);
+	ab2 = dot_pd(a_b, a_b);
+	a_p = vec3_sub(P, A);
+	ap = dot_pd(a_p, a_b);
+	t = ap / ab2;
+	if (t < 0)
 		t = 0;
-	else if (t > 1) // if the ratio is greater than 1, set the ratio to 1
+	else if (t > 1)
 		t = 1;
-	return (vec3_add(A, vec3_mult(a_b, t))); // return the closest point on the cylinder to the point of impact
+	return (vec3_add(A, vec3_mult(a_b, t)));
 }
 
-// get the normal of the cylinder at the point of impact
 static void	init_imp(t_impact *imp, double tmp, t_cylinder *cyl, t_ray r)
 {
-	imp->distance = tmp; // update the distance
-	imp->point = vec3_add(r.origin, vec3_mult(r.dir, tmp)); // get the point of impact
-	imp->normal = cyl->dir; // get the normal of the cylinder
+	imp->distance = tmp;
+	imp->point = vec3_add(r.origin, vec3_mult(r.dir, tmp));
+	imp->normal = cyl->dir;
 }
 
 void	cyl_ray(t_ray r, t_scene *sc, t_impact *imp, void **objs)
@@ -69,18 +67,18 @@ void	cyl_ray(t_ray r, t_scene *sc, t_impact *imp, void **objs)
 	{
 		is_side = 0;
 		cyl = (t_cylinder *)lst->content;
-		tmp = cylinder_intersection(r, *cyl, &is_side); // check if the ray hits the cylinder. If it does, update the impact
-		if (tmp < imp->distance && tmp > 0) // if the distance is less than the impact distance and greater than 0
+		tmp = cylinder_intersection(r, *cyl, &is_side);
+		if (tmp < imp->distance && tmp > 0)
 		{
-			*objs = cyl; // set the object to cylinder
-			init_imp(imp, tmp, cyl, r); // initialize the impact
-			if (is_side) // if the ray hits the side of the cylinder
+			*objs = cyl;
+			init_imp(imp, tmp, cyl, r);
+			if (is_side)
 				imp->normal = vec3_norm(vec3_sub(imp->point,
 							closest_point(cyl->pos, vec3_add(cyl->pos,
 									vec3_mult(cyl->dir, cyl->height)),
-								imp->point))); // get the normal of the cylinder at the point of impact
-			imp->normal = vec3_norm(imp->normal); // normalize the normal
-			imp->object = "cy"; // set the object to cylinder
+								imp->point)));
+			imp->normal = vec3_norm(imp->normal);
+			imp->object = "cy";
 		}
 		lst = lst->next;
 	}
